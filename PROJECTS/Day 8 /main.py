0a54b946863243f333a27d8b4bde6a8c8a43d5ad
Gabriel <gabriel.codeman@gmail.com>
2022-04-26 10:45:05 -0300

    Create main.py

---
+++ PROJECTS/Day 8 /main.py
@@ -0,0 +1,13 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type you message:\n")
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(tex, sft):
+    
+
+
+encrypt(tex=text, sft=shift)
